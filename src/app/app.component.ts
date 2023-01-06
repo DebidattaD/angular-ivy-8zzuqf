b0cc3ad218deb891462e6cd2de46a9188edf1a29
DebidattaD <120187185+DebidattaD@users.noreply.github.com>
2023-01-06 09:23:22 -0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component, VERSION } from '@angular/core';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'my-apps',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  name = 'Angular 2';
+  name: string = 'Angular2';
+  details: string = "start editing to see some magic happen!"
 }
